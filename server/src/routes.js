d65e125786dfce95d685a3ba0d31f42e5f09c90c
ArnavSharma005 <arnavoct.20@gmail.com>
2024-06-26 21:23:15 +0530

    add newsletter route with send-email functionality

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,12 @@
 // import authRoutes from "./router/auth.js";
-
+import newsRoutes from "./router/newsletter.js";
 function routes(app) {
     app.get("/", (req, res) => {
         res.send("Hello from server");
     });
 
     // app.use('/auth', authRoutes);
+    app.use("/newsletter", newsRoutes);
 }
 
 export default routes;
